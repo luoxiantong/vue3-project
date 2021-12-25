7a593853462eb9d3957a90b0912442c0c9a77522
罗贤通 <luoxiantong@enn.cn>
2021-12-25 09:12:47 +0800

    init

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,29 @@
-import { createStore } from 'vuex'
+import { InjectionKey } from 'vue'
+import { createStore, Store } from 'vuex'
+import user from "./modules/user";
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
+export interface State {
+  count: number
+}
+
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+  state() {
+    return {
+      count: 0
+    }
   },
   modules: {
+    user
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+    ,decreaseCrement(state) {
+      state.count--
+    }
+
   }
 })
---
+++ src/views/cart/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>cart</h1>
+  </div>
+</template>
